7d948bf3091be02ef42c260451da1f8de18020ab
Rawad Abi Naim Boukhouzam <rawadnm15@gmail.com>
2026-06-14 02:24:31 -0400

    Fix linting errors - Rawad

--- calculator.py
+++ calculator.py
@@ -1,9 +1,15 @@
 def add(a, b):
     return a + b
+
+
 def subtract(a, b):
     return a - b
+
+
 def multiply(a, b):
     return a * b
+
+
 def divide(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero")
